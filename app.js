8385a1a9281a5f65bce4cfd0c739537b7a2d29c4
Abel Costilla <aecostilla@gmail.com>
2017-06-16 17:24:04 -0500

    setup boiler plate

---
+++ app.js
@@ -0,0 +1,32 @@
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.config(function($routeProvider){
+  $routeProvider
+  
+    .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'weatherController'
+    })
+    
+    .when('/forecast', {
+      templateUrl: 'pages/forecast.html',
+      controller: 'forecastController'
+    })
+  
+});
+
+weatherApp.service('cityService', function(){
+    this.city = "New York, NY";
+});
+
+weatherApp.controller('weatherController', ['$scope', 'cityService', function($scope, cityService){
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+    });
+}]);
+
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+    $scope.city = cityService.city;
+}]);
